e4948cb4f489677d8a57a0ca94ad09795be2256d
danis <danis@DANIELLE-SANTIA>
2022-04-24 20:27:59 -0300

    Adicionar arquivos de projeto.

---
+++ Delivery/Delivery/Bolo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Delivery
+{
+    public class Bolo
+    {
+        List<Ingrediente> ingredientes;
+    }
+
+    public class Ingrediente
+    {
+        string nome = "Farinha de trigo";
+        string Cor = "branca";
+        decimal PesoEmGramas = 500;
+        decimal volumeEmMl = 0;
+    }
+}
---
+++ Delivery/Delivery/Program.cs
@@ -0,0 +1,33 @@
+﻿using Delivery;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        // Console.WriteLine("Hello, World!");
+
+        // var copoDeTexto = "água";
+
+        // int copoDeInt = 0;
+
+        // if (copoDeTexto == "água")
+        //     copoDeInt = 1;
+        // else
+        //     copoDeInt = 2;
+
+        // tipos de valor
+        // var variavelDeNumero = 1;
+
+        // int cloneVariavelDeNumero = variavelDeNumero;
+
+        // cloneVariavelDeNumero = 12;
+
+        // int outroCloneDeNumero = cloneVariavelDeNumero;
+
+        // Console.WriteLine(outroCloneDeNumero);
+
+        Bolo boloChocolate;
+        boloChocolate = new Bolo();
+        Console.WriteLine("teste de saída");
+    }
+}
